cdf695d5081cea85693e90d4e9702470826e8d40
Antihayp <zayzsacha1998@gmail.com>
2019-04-30 20:01:59 +0700

    Move triangles

--- 9.1lab/main.cpp
+++ 9.1lab/main.cpp
@@ -1,37 +1,4 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-using namespace glm;
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <sstream>
-
-int initGL();
-int initBuffer();
-int initColorBuffer();
-
-GLuint LoadShaders(const char *vertex_file_path,
-									const char *fragment_file_path);
-
-GLFWwindow *window;
-GLuint bufferID;
-GLuint colorBufferID;
-GLuint progHandle;
-GLuint vertexArrayID;
-
-const unsigned int window_width = 512;
-const unsigned int window_height = 512;
-
-const int num_of_verticies = 3;
+#include "main.hpp"
 
 int main()
 {
@@ -40,22 +7,67 @@ int main()
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	// glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
 	glGenVertexArrays(1, &vertexArrayID);
 	glBindVertexArray(vertexArrayID);
 
-	progHandle = LoadShaders("SimpleVertexShader.vertexshader",
-								"SimpleFragmentShader.fragmentshader");
+	progHandle = LoadShaders("SimpleVertexShader.glsl",
+								"SimpleFragmentShader.glsl");
 
-	initBuffer();
+	GLuint MatrixID = glGetUniformLocation(progHandle, "MVP");
+
+	glm::mat4 translateMatrix;
+	glm::mat4 MVP;
+
+	GLfloat valueX = 0.0f;
+	GLfloat valueY = 0.0f;
+
+	// initBuffer();
+	glGenBuffers(1, &bufferID);
+
+	glm::vec3 vertex_buffer_data[] = {
+		glm::vec3(-0.9f, -0.9f, -0.0f),
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(0.9f, -0.5f, 0.0f)
+	};
+
 	initColorBuffer();
 
 	do {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		translateMatrix = glm::translate(glm::mat4(1.0f),
+					glm::vec3(valueX, valueY, 0.0f));
+
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+			valueX -= 0.01f;
+			translateMatrix = glm::translate(glm::mat4(1.0f),
+					glm::vec3(valueX, valueY, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+			valueX += 0.01f;
+			translateMatrix = glm::translate(glm::mat4(1.0f),
+					glm::vec3(valueX, valueY, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+			valueY += 0.01f;
+			translateMatrix = glm::translate(glm::mat4(1.0f),
+					glm::vec3(valueX, valueY, 0.0f));
+		}
+		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+			valueY -= 0.01f;
+			translateMatrix = glm::translate(glm::mat4(1.0f),
+					glm::vec3(valueX, valueY, 0.0f));
+		}
+
+		MVP = translateMatrix;
+
+		glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data),
+								vertex_buffer_data, GL_STATIC_DRAW);
+
 		glUseProgram(progHandle);
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, bufferID);
@@ -113,18 +125,18 @@ int initBuffer()
 				};
 
 		glBufferData(GL_ARRAY_BUFFER, 6*num_of_verticies*sizeof(float),
-									vertex_buffer_data, GL_STATIC_DRAW );
+								vertex_buffer_data, GL_STATIC_DRAW );
 	#endif
 
 	#if 1
 		static const GLfloat vertex_buffer_data[] = { 
-				-0.9f, -0.9f, -0.0f,
+				-0.9f, -0.9f, -0.0f,	// left down
 				0.0f, 0.0f, 0.0f,
-				0.9f, -0.5f, 0.0f,
+				0.9f, -0.5f, 0.0f		// right down
 				};
 
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data),
-									vertex_buffer_data, GL_STATIC_DRAW);
+								vertex_buffer_data, GL_STATIC_DRAW);
 	#endif
 	
 	return 0;
@@ -136,14 +148,14 @@ int initColorBuffer()
 
 	glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
 
-	static const GLfloat vertex_buffer_data[] = { 
+	static const GLfloat color_buffer_data[] = { 
 			1.0f, 0.0f, 0.0f,
 			0.0f, 1.0f, 0.0f,
-			0.0f, 0.0f, 1.0f,
+			0.0f, 0.0f, 1.0f
 			};
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data),
-								vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data),
+								color_buffer_data, GL_STATIC_DRAW);
 
 	return 0;
 }
@@ -181,98 +193,4 @@ int initGL()
 	}
 
 	return 0;
-}
-
-GLuint LoadShaders(const char *vertex_file_path,
-										const char *fragment_file_path)
-{
-	// Create the shaders
-	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
-	// Read the Vertex Shader code from the file
-	std::string VertexShaderCode;
-	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-	if (VertexShaderStream.is_open()) {
-		std::stringstream sstr;
-		sstr << VertexShaderStream.rdbuf();
-		VertexShaderCode = sstr.str();
-		VertexShaderStream.close();
-	} else {
-		printf("Impossible to open %s. Are you in the right directory ?\
-			Don't forget to read the FAQ !\n", vertex_file_path);
-		getchar();
-		return 0;
-	}
-
-	// Read the Fragment Shader code from the file
-	std::string FragmentShaderCode;
-	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-	if (FragmentShaderStream.is_open()) {
-		std::stringstream sstr;
-		sstr << FragmentShaderStream.rdbuf();
-		FragmentShaderCode = sstr.str();
-		FragmentShaderStream.close();
-	}
-
-	GLint Result = GL_FALSE;
-	int InfoLogLength;
-
-	// Compile Vertex Shader
-	printf("Compiling shader : %s\n", vertex_file_path);
-	char const * VertexSourcePointer = VertexShaderCode.c_str();
-	glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
-	glCompileShader(VertexShaderID);
-
-	// Check Vertex Shader
-	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL,
-									&VertexShaderErrorMessage[0]);
-		printf("%s\n", &VertexShaderErrorMessage[0]);
-	}
-
-	// Compile Fragment Shader
-	printf("Compiling shader : %s\n", fragment_file_path);
-	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
-	glCompileShader(FragmentShaderID);
-
-	// Check Fragment Shader
-	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL,
-										&FragmentShaderErrorMessage[0]);
-		printf("%s\n", &FragmentShaderErrorMessage[0]);
-	}
-
-	// Link the program
-	printf("Linking program\n");
-	GLuint ProgramID = glCreateProgram();
-	glAttachShader(ProgramID, VertexShaderID);
-	glAttachShader(ProgramID, FragmentShaderID);
-	glLinkProgram(ProgramID);
-
-	// Check the program
-	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
-		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL,
-												&ProgramErrorMessage[0]);
-		printf("%s\n", &ProgramErrorMessage[0]);
-	}
-
-	
-	glDetachShader(ProgramID, VertexShaderID);
-	glDetachShader(ProgramID, FragmentShaderID);
-	
-	glDeleteShader(VertexShaderID);
-	glDeleteShader(FragmentShaderID);
-
-	return ProgramID;
 }
---
+++ 9.1lab/main.hpp
@@ -0,0 +1,33 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+// #include <glm/gtx/transform.hpp>
+// using namespace glm;
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "shader.hpp"
+
+GLFWwindow *window;
+GLuint bufferID;
+GLuint colorBufferID;
+GLuint progHandle;
+GLuint vertexArrayID;
+
+const unsigned int window_width = 1024;
+const unsigned int window_height = 768;
+
+const int num_of_verticies = 3;
+
+int initGL();
+int initBuffer();
+int initColorBuffer();
+
+#endif
