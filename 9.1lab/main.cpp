80fd6619938310627b22fb6b5e385f539ae009f9
Antihayp <zayzsacha1998@gmail.com>
2019-05-01 23:22:17 +0700

    Tetrahedron

--- 9.1lab/main.cpp
+++ 9.1lab/main.cpp
@@ -22,6 +22,9 @@ int main()
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 	// glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GLFW_TRUE);
 
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
 	glGenVertexArrays(1, &vertexArrayID);
 	glBindVertexArray(vertexArrayID);
 
@@ -51,11 +54,76 @@ int main()
 	// initBuffer();
 	glGenBuffers(1, &bufferID);
 
+	#if 0
 	glm::vec3 vertex_buffer_data[] = {
 		glm::vec3(-0.9f, -0.9f, -0.0f),
 		glm::vec3(0.0f, 0.2f, 0.0f),
 		glm::vec3(0.9f, -0.5f, 0.0f)
 	};
+	#endif
+
+	#if 1
+	glm::vec3 vertex_buffer_data[] = {
+		glm::vec3(-0.9f, -0.9f, -0.0f),
+		glm::vec3(0.0f, 0.2f, 0.0f),
+		glm::vec3(0.9f, -0.5f, 0.0f),
+		glm::vec3(-0.9f, -0.9f, -0.0f),
+		glm::vec3(0.0f, 0.2f, 0.0f),
+		glm::vec3(0.0f, 0.0f, 0.5f),
+		glm::vec3(-0.9f, -0.9f, -0.0f),
+		glm::vec3(0.9f, -0.5f, 0.0f),
+		glm::vec3(0.0f, 0.0f, 0.5f),
+		glm::vec3(0.0f, 0.2f, 0.0f),
+		glm::vec3(0.9f, -0.5f, 0.0f),
+		glm::vec3(0.0f, 0.0f, 0.5f)
+	};
+	#endif
+
+	#if 0
+	static GLfloat vertex_buffer_data[] = {
+		-1.0f,-1.0f,-1.0f, // Треугольник 1 : начало
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, // Треугольник 1 : конец
+		1.0f, 1.0f,-1.0f, // Треугольник 2 : начало
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f, // Треугольник 2 : конец
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
+	};
+    #endif
+
+    glBindBuffer(GL_ARRAY_BUFFER, bufferID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data),
+								vertex_buffer_data, GL_STATIC_DRAW);
+
 
 	initColorBuffer();
 
@@ -70,16 +138,11 @@ int main()
 		// translateTriangles();
 		testRotateMatrix();
 
-		// rotateMatrix = glm::rotate(glm::mat4(1.0f),
-								// angle, glm::vec3(0.0f, 0.0f, 1.0f));
-
-		// angle += 0.01f;
-
 		// MVP = translateMatrix * scaleMatrix;
 		// MVP = translateMatrix;
 		// MVP = rotateMatrix;
 
-		#if 1
+		#if 0
 		printf("\n");
 		for (int i = 0; i < 4; i++) {
 			printf("%f %f %f %f\n",
@@ -87,10 +150,6 @@ int main()
 		}
 		printf("\n");
 		#endif
-
-		glBindBuffer(GL_ARRAY_BUFFER, bufferID);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data),
-								vertex_buffer_data, GL_STATIC_DRAW);
 
 		glUseProgram(progHandle);
 
@@ -112,7 +171,7 @@ int main()
 		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3 * 4);
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
@@ -251,11 +310,71 @@ int initColorBuffer()
 
 	glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
 
+	#if 0
 	static const GLfloat color_buffer_data[] = { 
-			1.0f, 0.0f, 0.0f,
-			0.0f, 1.0f, 0.0f,
-			0.0f, 0.0f, 1.0f
-			};
+		1.0f, 0.0f, 0.0f,	// read
+		0.0f, 1.0f, 0.0f,	// green
+		0.0f, 0.0f, 1.0f 	// blue
+	};
+	#endif
+
+	#if 1
+	static const GLfloat color_buffer_data[] = { 
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f
+	};
+	#endif
+
+	#if 0
+	static const GLfloat color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+	#endif
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data),
 								color_buffer_data, GL_STATIC_DRAW);
